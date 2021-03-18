b9d2b4674b059a0607f4e277895b836fe45d6a3d
Ocel117 <ocalruvi117@outlook.com>
2021-03-17 22:34:24 -0600

    Perdicion

--- Autest.java
+++ Autest.java
@@ -1,3 +1,5 @@
+import javafx.animation.FadeTransition;
+
 public class Autest {
     public static void main (String[] args){
         System.out.println("Tenemos los siguientes Autos\n\n");
@@ -12,7 +14,26 @@ public class Autest {
         compactos.marca = "Ford";
         compactos.color = "Negro";
 
-        compactos.Caracteristicas();
+        compactos.Caracteristicas("Ford FIGO");
 
+        carreras.puertas = 2;
+        carreras.noPlaca = "OFHISU89-7Y23E23";
+        carreras.velocidad = 152;
+        carreras.faros = false;
+        carreras.marca = "Mercedez Benz";
+        carreras.color = "Rojo";
+
+        carreras.Caracteristicas("300 SL (M.1952)");
+
+        combertibles.puertas = 4;
+        combertibles.noPlaca = "FHGBNIF-FGYH78F";
+        combertibles.velocidad = 100;
+        combertibles.faros = true;
+        combertibles.marca = "Toyota";
+        combertibles.color = "Rosa";
+
+        combertibles.Caracteristicas("Mr2 Spyder");
+        combertibles.Direccion(15, 45);
+        combertibles.Ubicacion("Narnia");
     }    
 }
--- Auto.java
+++ Auto.java
@@ -7,13 +7,13 @@ public class Auto{
     public String color;
 
     public void Direccion(int derecha, int izquierda){
-        System.out.println("Lado derecho: "+derecha+"\nLado izquierdo: "+izquierda);
+        System.out.println("Lado derecho: "+derecha+"\nLado izquierdo: "+izquierda+"\n");
     } 
     public void Freno(boolean chino){
-        System.out.println("El freno esta activo? " + chino);
+        System.out.println("El freno esta activo? " + chino+"\n");
     }
     String X = "Algo";
-    public void Caracteristicas(){
+    public void Caracteristicas(String auto){
         
         if (faros == true){
             X = "encendido";
@@ -21,15 +21,19 @@ public class Auto{
         else{
             X = "apagado";
         }
+        System.out.println("\n\n\n");
+        System.out.println("El auto es: " + auto);
+        System.out.println("\n");
         System.out.println("No. de puertas: " + puertas);
         System.out.println("No. de placa: " + noPlaca);
         System.out.println("Km/h: " + velocidad);
         System.out.println("Faros: " + X);
         System.out.println("Marca: " + marca);
         System.out.println("Color: " + color);
+        System.out.println("\n");
     }
     public void Ubicacion(String lugar){
-        System.out.println("Usted esta perdido en: " + lugar);
+        System.out.println("Usted esta perdido en: " + lugar+"\n");
     }
-
+    
 }
