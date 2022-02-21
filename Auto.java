69c354fe5c21d91ce64b5e7c88905a48b202d1db
Ocel Alfredo Ruiz Villeda <67806170+Ocel117@users.noreply.github.com>
2022-02-20 20:07:28 -0600

    update of attributes and methods

--- Auto.java
+++ Auto.java
@@ -4,9 +4,11 @@ public class Auto{
     public  int puertas;
     public String noPlaca;
     public float velocidad;
-    public boolean faros;
+    public int cilindros;
     public String marca;
     public String color;
+    public int llantas;
+    public boolean faros;
 
     // Metodos
     public void direccion(int derecha, int izquierda){
@@ -28,15 +30,22 @@ public class Auto{
         System.out.println("El auto es: " + auto);
         System.out.println("\n");
         System.out.println("No. de puertas: " + puertas);
+        System.out.println("No. de llantas: " + llantas);
         System.out.println("No. de placa: " + noPlaca);
         System.out.println("Km/h: " + velocidad);
         System.out.println("Faros: " + X);
         System.out.println("Marca: " + marca);
         System.out.println("Color: " + color);
+        System.out.println("No. de cilindros: " + cilindros);
         System.out.println("\n");
     }
+        //La ubicacion es para burlarse de la persona que esta perdida y dejarla a su suerte
     public void ubicacion(String lugar){
         System.out.println("Usted esta perdido en: " + lugar+"\n");
+    }
+        //Tipo de auto ejemplo: deportivo, todoterreno, etc.
+    plublic void autoTipo(String tipo){
+        System.out.println("Ël auto es: " + tipo + "\n");
     }
     
 }
